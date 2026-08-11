d3114930ba90d28cbd754ef9e286cf68627df6cb
kakusika <329925104+kakusika@users.noreply.github.com>
2026-08-12 02:49:52 +0900

    !migrate to flake-parts

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     treefmt-nix.url = "github:numtide/treefmt-nix";
 
     #[ Rust ]
